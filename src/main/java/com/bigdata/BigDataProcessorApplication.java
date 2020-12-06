764e678450c17b413218cee45d54aefc12ab699e
Marguerite Blair <marguerite.blair@gmail.com>
2020-12-06 00:04:08 -0500

    Initial Commit

---
+++ src/main/java/com/bigdata/BigDataProcessorApplication.java
@@ -0,0 +1,13 @@
+package com.bigdata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BigDataProcessorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BigDataProcessorApplication.class, args);
+	}
+
+}
