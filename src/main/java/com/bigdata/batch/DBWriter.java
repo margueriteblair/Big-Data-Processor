292a9825588829ffd4fe5447c4e6d28e1ede0e3c
Marguerite Blair <marguerite.blair@gmail.com>
2020-12-06 00:34:05 -0500

    created a db writer

---
+++ src/main/java/com/bigdata/batch/DBWriter.java
@@ -0,0 +1,19 @@
+package com.bigdata.batch;
+
+import org.springframework.batch.item.ItemWriter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+public class DBWriter implements ItemWriter {
+
+    @Autowired
+    private TransactionRepository repository;
+
+    @Override
+    public void write(List<? extends Transaction> transactions) throws Exception) {
+        repository.saveAll(transactions);
+    }
+}
