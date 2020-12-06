5334d61b4ca2b03205111a5fdce20257ac281ca5
Marguerite Blair <marguerite.blair@gmail.com>
2020-12-06 10:49:49 -0500

    function that allows a small new file to be created from the data that were currently chunking from the csv

--- src/main/java/com/bigdata/utils/ChunkFile.java
+++ src/main/java/com/bigdata/utils/ChunkFile.java
@@ -2,6 +2,8 @@ package com.bigdata.utils;
 
 import org.springframework.core.io.ClassPathResource;
 
+import java.io.*;
+
 public class ChunkFile {
     private static final int COUNT = 2;
 
@@ -14,5 +16,22 @@ public class ChunkFile {
             ex.printStackTrace();
         }
 
+    }
+
+    public static void writeSmallFile(String text, int fileCount) throws IOException {
+        String fileNameSuffix = "PS_Sample_log_Part_";
+        String newFileName = fileNameSuffix + fileCount + ".csv";
+        File file = new File(newFileName);
+        if (!file.exists()) {
+            file.createNewFile();
+        }
+
+        try (BufferedWriter out = new BufferedWriter(new OutputStreamWriter(
+                new FileOutputStream(newFileName), "UTF-8"))) {
+            out.write(text.toString());
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
