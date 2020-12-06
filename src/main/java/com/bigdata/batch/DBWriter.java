d67da15cbc1c1f351bd9524e35fde26d329c45bf
Marguerite Blair <marguerite.blair@gmail.com>
2020-12-06 01:43:03 -0500

    proper dependencies added and deleted

--- src/main/java/com/bigdata/batch/DBWriter.java
+++ src/main/java/com/bigdata/batch/DBWriter.java
@@ -1,6 +1,7 @@
 package com.bigdata.batch;
 
 import com.bigdata.model.Transaction;
+import com.bigdata.repository.TransactionRepository;
 import org.springframework.batch.item.ItemWriter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -12,9 +13,14 @@ public class DBWriter implements ItemWriter {
 
     @Autowired
     private TransactionRepository repository;
+//
+//    @Override
+//    public void write(List<? extends Transaction> transactions) throws Exception {
+//        repository.saveAll(transactions);
+//    }
 
     @Override
-    public void write(List<? extends Transaction> transactions) throws Exception) {
-        repository.saveAll(transactions);
+    public void write(List list) throws Exception {
+
     }
 }
--- src/main/java/com/bigdata/services/ImportServiceProxy.java
+++ src/main/java/com/bigdata/services/ImportServiceProxy.java
@@ -1,7 +1,9 @@
 package com.bigdata.services;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.cloud.openfeign.FeignClient;
 
+@FeignClient(name="bigdata-reports-service", fallback=ReportsFallback.class)
 public interface ImportServiceProxy {
 
     @GetMapping("api/report/fraud")
