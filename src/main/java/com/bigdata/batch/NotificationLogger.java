469d2bc305a8034561ffa3d43eea31f435f9d354
Marguerite Blair <marguerite.blair@gmail.com>
2020-12-06 00:31:34 -0500

    class for when the report is done

---
+++ src/main/java/com/bigdata/batch/NotificationLogger.java
@@ -0,0 +1,30 @@
+package com.bigdata.batch;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.batch.core.BatchStatus;
+import org.springframework.batch.core.JobExecution;
+import org.springframework.batch.core.listener.JobExecutionListenerSupport;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+public class NotificationLogger extends JobExecutionListenerSupport {
+    private static final Logger logger = LoggerFactory.getLogger(NotificationLogger.class
+    );
+
+    @Autowired
+    ThreadPoolTaskExecutor taskExecutor;
+
+    @Autowired
+    private ImportServiceProxy importServiceProxy;
+
+    @Override
+    public void afterRun(JobExecution jobExecution) {
+        if (jobExecution.getStatus() == BatchStatus.COMPLETED) {
+            logger.info("Job is completed. Time to call Reports Service.");
+
+            String res = importServiceProxy.generateReports();
+            logger.info("report service: ", res);
+        }
+    }
+}
