18ecbb12d921d2d61ce0a371b33c36fbb020d8fc
Marguerite Blair <marguerite.blair@gmail.com>
2020-12-07 22:13:01 -0500

    tx model and constructor

--- src/main/java/com/bigdata/model/Transaction.java
+++ src/main/java/com/bigdata/model/Transaction.java
@@ -15,4 +15,22 @@ public class Transaction {
     private BigDecimal newBalanceDest;
     private Boolean isFraud;
     private Boolean isFlaggedFraud;
+
+    public Transaction() {}
+
+    public Transaction(int step, String type, BigDecimal amount, String nameOrig, BigDecimal oldBalanceOrg, BigDecimal newBalanceOrig, String nameDest,
+                BigDecimal oldBalanceDest, BigDecimal newBalanceDest, Boolean isFraud, Boolean isFlaggedFraud
+    ) {
+        this.step = step;
+        this.type = type;
+        this.amount = amount;
+        this.nameOrig = nameOrig;
+        this.oldBalanceOrg = oldBalanceOrg;
+        this.newBalanceOrig = newBalanceOrig;
+        this.nameDest = nameDest;
+        this.oldBalanceDest = oldBalanceDest;
+        this.newBalanceDest = newBalanceDest;
+        this.isFraud = isFraud;
+        this.isFlaggedFraud = isFlaggedFraud;
+    }
 }
