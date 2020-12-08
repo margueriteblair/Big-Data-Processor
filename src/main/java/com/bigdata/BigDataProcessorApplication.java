17d4529259a0845be3c01bb5a80255f2575ffd6c
Marguerite Blair <marguerite.blair@gmail.com>
2020-12-07 21:57:47 -0500

    edited a ton of dependencies to not be read so that the server can run while we dont have our db in place yet

--- src/main/java/com/bigdata/BigDataProcessorApplication.java
+++ src/main/java/com/bigdata/BigDataProcessorApplication.java
@@ -2,12 +2,8 @@ package com.bigdata;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//import  org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
-//@EnableDiscoveryClient
-//@EnableFeignClients
-//@EnableCircuitBreaker
 public class BigDataProcessorApplication {
 
 	public static void main(String[] args) {
