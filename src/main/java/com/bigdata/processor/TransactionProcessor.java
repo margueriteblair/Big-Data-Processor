1e6c3f9a073d2755b4a3a19aea2062691d5b9ec0
Marguerite Blair <marguerite.blair@gmail.com>
2020-12-07 22:27:53 -0500

    setting up a tx processor that implements item processor

---
+++ src/main/java/com/bigdata/processor/TransactionProcessor.java
@@ -0,0 +1,9 @@
+package com.bigdata.processor;
+
+import com.bigdata.model.Transaction;
+import org.springframework.batch.item.ItemProcessor;
+
+public class TransactionProcessor implements ItemProcessor<Transaction, Transaction> {
+
+
+}
