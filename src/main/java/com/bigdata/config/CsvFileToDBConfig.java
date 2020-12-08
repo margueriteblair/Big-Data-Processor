723368a41d891df5695b1c844fd27ce47bb852b0
Marguerite Blair <marguerite.blair@gmail.com>
2020-12-07 22:40:00 -0500

    reader writer and processor

---
+++ src/main/java/com/bigdata/config/CsvFileToDBConfig.java
@@ -0,0 +1,21 @@
+package com.bigdata.config;
+
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import javax.sql.DataSource;
+
+public class CsvFileToDBConfig {
+
+    @Autowired
+    public JobBuilderFactory jobBuilderFactory;
+
+    @Autowired
+    public StepBuilderFactory stepBuilderFactory;
+
+    @Autowired
+    public DataSource dataSource;
+
+    //below here we start the reader writer and processor
+}
