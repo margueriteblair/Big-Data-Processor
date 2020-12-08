0e900fca28a1d40bdfc876a9555103311297be3e
Marguerite Blair <marguerite.blair@gmail.com>
2020-12-07 21:53:36 -0500

    starting fresh from square 1

--- src/main/java/com/bigdata/repository/TransactionRepository.java
+++
@@ -1,7 +0,0 @@
-package com.bigdata.repository;
-
-import com.bigdata.model.Transaction;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TransactionRepository extends JpaRepository<Transaction, String> {
-}
