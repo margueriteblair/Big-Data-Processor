be8fbd112a3fa41342b04a18b74bcb008a874ab1
Marguerite Blair <marguerite.blair@gmail.com>
2020-12-10 01:52:14 -0500

    created jpa model

---
+++ src/main/java/com/bigdata/repository/TransactionRepository.java
@@ -0,0 +1,7 @@
+package com.bigdata.repository;
+
+import com.bigdata.model.Transaction;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TransactionRepository extends JpaRepository<Transaction, Long> {
+}
