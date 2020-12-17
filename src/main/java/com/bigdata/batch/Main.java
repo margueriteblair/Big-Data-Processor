3db739d613e6ebca8d597f5b70c9e167b33bfb8b
Marguerite Blair <marguerite.blair@gmail.com>
2020-12-16 21:49:44 -0500

    built a main class for the main batch ops

---
+++ src/main/java/com/bigdata/batch/Main.java
@@ -0,0 +1,13 @@
+package com.bigdata.batch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Main {
+
+    public static void main(String[] args) {
+        SpringApplication.run(com.bigdata.batch.Main.class, args);
+    }
+
+}
