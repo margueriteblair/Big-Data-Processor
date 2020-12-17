c926ee6b653dfc81f3f38aaf1bce693055308aeb
Marguerite Blair <marguerite.blair@gmail.com>
2020-12-16 22:02:34 -0500

    get request

--- src/main/java/com/bigdata/batch/controller/LoadController.java
+++ src/main/java/com/bigdata/batch/controller/LoadController.java
@@ -1,11 +1,18 @@
 package com.bigdata.batch.controller;
 
 
-import org.springframework.batch.core.Job;
+import org.springframework.batch.core.*;
 import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.batch.core.repository.JobExecutionAlreadyRunningException;
+import org.springframework.batch.core.repository.JobInstanceAlreadyCompleteException;
+import org.springframework.batch.core.repository.JobRestartException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/load")
@@ -16,4 +23,22 @@ public class LoadController {
 
     @Autowired
     Job job;
+
+    @GetMapping
+    public BatchStatus load() throws JobParametersInvalidException, JobExecutionAlreadyRunningException, JobRestartException, JobInstanceAlreadyCompleteException {
+        Map<String, JobParameter> maps = new HashMap<>();
+        maps.put("time", new JobParameter(System.currentTimeMillis()));
+        JobParameters parameters = new JobParameters(maps);
+        JobExecution jobExecution = jobLauncher.run(job, parameters);
+
+        System.out.println("Job Execution: " + jobExecution.getStatus() + jobExecution.getAllFailureExceptions() + "\n"
+                + jobExecution.getFailureExceptions() + jobExecution.getExitStatus()
+        );
+
+        System.out.println("The batch is running!");
+        while (jobExecution.isRunning()) {
+            System.out.println("...");
+        }
+        return jobExecution.getStatus();
+    }
 }
