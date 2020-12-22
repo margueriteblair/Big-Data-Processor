87f2c15ad065cc4fbbc627d8289e8296044f24ea
Marguerite Blair <marguerite.blair@gmail.com>
2020-12-22 01:28:00 -0500

    try catch block for actually reading and splitting the large file

--- src/main/java/com/bigdata/batch/utils/ChunkBigFile.java
+++ src/main/java/com/bigdata/batch/utils/ChunkBigFile.java
@@ -1,5 +1,11 @@
 package com.bigdata.batch.utils;
 
+import org.springframework.core.io.ClassPathResource;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class ChunkBigFile {
 
     private static final int SPLIT_COUNT = 2;
@@ -8,7 +14,16 @@ public class ChunkBigFile {
         readLargeFileAndChunk();
     }
 
-    public static void readLargeFileAndChunk() {
+    public static void readLargeFileAndChunk() throws IOException {
+        StringBuilder text = new StringBuilder();
 
+        ClassPathResource input = new ClassPathResource("data/PS_20174392719_1491204439457_log");
+        try (BufferedReader in = new BufferedReader(new InputStreamReader(
+                input.getInputStream()))) {
+            int count = 0;
+            int fileCount = 0;
+            String line = in.readLine();
+
+        }
     }
 }
