8f34ace0ae2d3147d6daf43c7f179e567e9141ae
Marguerite Blair <marguerite.blair@gmail.com>
2020-12-22 01:25:40 -0500

    trying to make a separate util for chunking data

---
+++ src/main/java/com/bigdata/batch/utils/ChunkBigFile.java
@@ -0,0 +1,14 @@
+package com.bigdata.batch.utils;
+
+public class ChunkBigFile {
+
+    private static final int SPLIT_COUNT = 2;
+
+    public static void startSplit() {
+        readLargeFileAndChunk();
+    }
+
+    public static void readLargeFileAndChunk() {
+
+    }
+}
